37a29322a76557658d68c91c8e64178d7f5e15a6
Sanchya Sahay <ss5901@columbia.edu>
2019-11-27 00:37:42 +0000

    Sample data and template and view for /sightings/

--- tracker/views.py
+++ tracker/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Squirrel
 
 def sightings(request):
-    return HttpResponse("The list of squirrel sightings will go here")
+    squirrels = Squirrel.objects.all()
+    context={
+            'squirrels':squirrels,
+         }
+    return render(request, 'tracker/sightings.html', context)
+
+
 
 def map(request):
     return HttpResponse("The squirrel map will go here")
 
-def sighting_update(request):
+def sighting_update(request, unique_squirrel_id):
     return HttpResponse ("Hi, I'm squirrel.}")
 
 def new_sighting(request):
-    return HttpResponse("Here's where we'll add a sighting!")
+    return HttpResponse("Here's where a new sighting!")
 
 def stats(request): 
     return HttpResponse("Here are some stats.") 
